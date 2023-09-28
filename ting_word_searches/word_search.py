fd9da57fa614aa08af33ed9593b46820fbf86eee
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-28 19:20:15 -0300

    Feat: Cria função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,16 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    items_with_word = []
+    for item in instance.queue:
+        lines_with_word = [i+1 for i, quote in enumerate(
+            item["linhas_do_arquivo"]) if word.lower() in quote.lower()]
+        if len(lines_with_word) > 0:
+            item_with_word = {
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": [{"linha": line} for line in lines_with_word]
+            }
+            items_with_word.append(item_with_word)
+    return items_with_word
 
 
 def search_by_word(word, instance):
