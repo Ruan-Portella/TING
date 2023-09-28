b23ed87b0eb633686458aaf8bbcf13856187a9ac
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-28 15:01:11 -0300

    Feat: Cria arquivo queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,24 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
+
+    def is_empty(self):
+        return len(self.queue) == 0
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
+        return True
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            return None
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self.queue):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.queue[index]
