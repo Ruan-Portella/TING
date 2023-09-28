4499ac279241b113eb6c6fc948019c0bc516a6d2
Ruan-Portella <ruanmorales29@gmail.com>
2023-09-28 15:12:23 -0300

    Feat: Cria função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith('.txt'):
+        try:
+            with open(path_file, 'r') as file:
+                return [line.replace("\n", "") for line in file.readlines()]
+        except FileNotFoundError:
+            return print(f"Arquivo {path_file} não encontrado",
+                         file=sys.stderr)
+    else:
+        return print("Formato inválido", file=sys.stderr)
