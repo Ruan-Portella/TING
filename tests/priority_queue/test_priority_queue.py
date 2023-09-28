1b034d48b646e21cbdadf3cf12179c1c2a7925b5
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-09-27 21:41:19 -0300

    Initial commit

---
+++ tests/priority_queue/test_priority_queue.py
@@ -0,0 +1,5 @@
+# from ting_file_management.priority_queue import PriorityQueue
+
+
+def test_basic_priority_queueing():
+    """Aqui irá sua implementação"""
---
+++ ting_file_management/queue.py
@@ -0,0 +1,18 @@
+from ting_file_management.abstract_queue import AbstractQueue
+
+
+class Queue(AbstractQueue):
+    def __init__(self):
+        """Inicialize sua estrutura aqui"""
+
+    def __len__(self):
+        """Aqui irá sua implementação"""
+
+    def enqueue(self, value):
+        """Aqui irá sua implementação"""
+
+    def dequeue(self):
+        """Aqui irá sua implementação"""
+
+    def search(self, index):
+        """Aqui irá sua implementação"""
